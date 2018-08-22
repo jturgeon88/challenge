3a97d11c4fabecf0ed64d9c17919fb8ece8e64c4
Josh Turgeon <j_turgeon88@yahoo.com>
2018-08-22 14:20:03 -0500

    Test that TimerHistory is passing deleteTimeEntry function to TimerHistoryItem as props;

--- test/frontend/components/TimerHistory.test.js
+++ test/frontend/components/TimerHistory.test.js
@@ -7,4 +7,15 @@ describe('TimerHistory Component', () => {
   it('renders without crashing', () => {
     shallow(<TimerHistory timeEntries={timeEntrySeeds()} />);
   });
+
+  it('should pass deleteTimeEntry as props to TimerHistoryItem', () => {
+    const deleteTimeEntry = jest.fn();
+    const wrapper = shallow(<TimerHistory
+                              timeEntries={timeEntrySeeds()}
+                              deleteTimeEntry={deleteTimeEntry}
+                            />);
+    const firstTimerHistoryItem = wrapper.find('TimerHistoryItem').at(0);
+
+    expect(firstTimerHistoryItem.props().deleteTimeEntry).toBe(deleteTimeEntry);
+  })
 });
