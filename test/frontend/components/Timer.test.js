10507799e0af30fc76266310f981799b4346b8b0
Josh Turgeon <j_turgeon88@yahoo.com>
2018-08-24 14:24:16 -0500

    Test that setDescription is called when description field is changed

--- test/frontend/components/Timer.test.js
+++ test/frontend/components/Timer.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import TimeEntryForm from '../../../src/components/TimeEntryForm';
 import { createTimeEntry } from '../../../src/utils/timerUtils';
+import Task from '../../../src/components/Task';
 
 let addTimeEntry;
 
@@ -27,5 +28,19 @@ describe('TimeEntryForm', () => {
 
     wrapper.instance().setDescription(newDescription);
     expect(wrapper.state().description).toEqual('Best Description Ever');
+  });
+});
+
+describe('Task', () => {
+  it('calls setDescription when field is changed', () => {
+    const setDescription = jest.fn();
+    const wrapper = shallow(<Task setDescription={setDescription} />);
+    const event = {
+      preventDefault: () => {},
+      currentTarget: {value: 'This is a change'}
+    };
+
+    wrapper.find('input').simulate('change', event);
+    expect(setDescription).toHaveBeenCalled();
   });
 });
